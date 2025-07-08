dd376004c5e9456dcecfa1e78add32fd360d5108
Piotr Równicki <piotr.rownicki@gearcode.eu>
2025-07-08 12:06:39 +0200

    increased world map resolution

--- GeoJourneyer/GeoJourneyer.App/GeoJourneyer.App.Shared/wwwroot/globe.js
+++ GeoJourneyer/GeoJourneyer.App/GeoJourneyer.App.Shared/wwwroot/globe.js
@@ -6,7 +6,7 @@ window.initGlobe = function (visited, dotNetHelper) {
         window.globeInstance = Globe()(container)
             .globeImageUrl(null)
             .bumpImageUrl(null);
-        fetch('https://unpkg.com/world-atlas@2.0.2/countries-50m.json')
+        fetch('https://unpkg.com/world-atlas@2.0.2/countries-10m.json')
             .then(res => res.json())
             .then(worldData => {
                 const countries = window.topojson.feature(worldData, worldData.objects.countries).features;
