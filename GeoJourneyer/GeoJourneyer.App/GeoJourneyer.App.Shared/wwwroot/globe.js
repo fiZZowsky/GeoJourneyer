e12ae21f293c5d6d84f005edc45c099cdd288305
Piotr Równicki <piotr.rownicki@gearcode.eu>
2025-07-09 12:47:18 +0200

    added the option to select a country from the list when registering a user, globe.js code improvement

--- GeoJourneyer/GeoJourneyer.App/GeoJourneyer.App.Shared/wwwroot/globe.js
+++ GeoJourneyer/GeoJourneyer.App/GeoJourneyer.App.Shared/wwwroot/globe.js
@@ -2,45 +2,52 @@ window.initGlobe = function (visited, dotNetHelper) {
     const container = document.getElementById('globeContainer');
     if (!container) return;
 
-    if (!window.globeInstance) {
-        window.globeInstance = Globe()(container)
-            .globeImageUrl(null)
-            .bumpImageUrl(null);
+    container.innerHTML = '';
+    window.globeInstance = Globe()(container)
+        .globeImageUrl(null)
+        .bumpImageUrl(null);
+
+    const setupGlobe = (countries) => {
+        window.countriesData = countries;
+        window.globeInstance
+            .polygonsData(countries)
+            .polygonAltitude(0.06)
+            .polygonSideColor(() => 'rgba(0,0,0,0)')
+            .polygonStrokeColor(() => '#000')
+            .onPolygonClick(d => {
+                const name = d.properties.name;
+                if (!Array.isArray(window.visitedCountries)) {
+                    window.visitedCountries = [];
+                }
+                if (window.visitedCountries.indexOf(name) >= 0) {
+                    window.visitedCountries = window.visitedCountries.filter(c => c !== name);
+                } else {
+                    window.visitedCountries.push(name);
+                }
+                window.updateVisitedCountries(window.visitedCountries);
+                if (window.dotNetHelper) {
+                    window.dotNetHelper.invokeMethodAsync('ToggleCountry', name);
+                }
+            });
+
+        window.updateVisitedCountries(visited || []);
+        // TODO: not wordking
+        //if (dotNetHelper) {
+        //    dotNetHelper.invokeMethodAsync('GlobeLoaded');
+        //}
+    };
+
+    if (!window.countriesData) {
         fetch('_content/GeoJourneyer.App.Shared/countries-50m.json')
             .then(res => res.json())
             .then(worldData => {
                 const countries = window.topojson.feature(worldData, worldData.objects.countries).features;
-                window.countriesData = countries;
-                window.globeInstance
-                    .polygonsData(countries)
-                    .polygonAltitude(0.06)
-                    .polygonSideColor(() => 'rgba(0,0,0,0)')
-                    .polygonStrokeColor(() => '#000')
-                    .onPolygonClick(d => {
-                        const name = d.properties.name;
-                        if (!Array.isArray(window.visitedCountries)) {
-                            window.visitedCountries = [];
-                        }
-                        if (window.visitedCountries.indexOf(name) >= 0) {
-                            window.visitedCountries = window.visitedCountries.filter(c => c !== name);
-                        } else {
-                            window.visitedCountries.push(name);
-                        }
-                        window.updateVisitedCountries(window.visitedCountries);
-                        if (window.dotNetHelper) {
-                            window.dotNetHelper.invokeMethodAsync('ToggleCountry', name);
-                        }
-                    });
-
-                window.updateVisitedCountries(visited || []);
-                // TODO: not wordking
-                //if (dotNetHelper) {
-                //    dotNetHelper.invokeMethodAsync('GlobeLoaded');
-                //}
-            })
+                setupGlobe(countries);
+            });
     } else {
-        window.updateVisitedCountries(visited || []);
+        setupGlobe(window.countriesData);
     }
+
     window.dotNetHelper = dotNetHelper;
 }
 
